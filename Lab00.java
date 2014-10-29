be2756efd86bdc5e6548764b99cb30911b1ad5f9
shaurya-prakash <shaurya_prakash@asl.org>
2014-10-29 17:18:44 +0000

    Lab00

--- Lab00.java
+++ Lab00.java
@@ -1,9 +1,9 @@
 /**
 *
-* Description of the program goes here  // provide a brief description
+* First Program (Lab00 in Unit1, fcps.karel)
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author Shaurya Prakash
+* @version 22/10/2014
 */
 
 import edu.fcps.karel2.Display;
@@ -11,12 +11,23 @@ import edu.fcps.karel2.Robot;
 
 public class Lab00 {
 
-     public static void main(String[] args) {
-             Display.openWorld("maps/first.map");
-   Display.setSize(10, 10);
-   Display.setSpeed(3);
-   Robot karel = new Robot();
-   karel.move();
-   karel.move();
+     public static void main(String[] args) 
+     {
+             
+         Display.openWorld("maps/first.map");
+         Display.setSize(10, 10);
+         Display.setSpeed(5);
+   
+         Robot karel = new Robot();
+  
+         karel.move();
+         karel.pickBeeper();
+         karel.move();
+         karel.turnLeft();
+         karel.move();
+         karel.putBeeper();
+         karel.move();
+         karel.turnLeft();
+         karel.turnLeft();
      }
 }
